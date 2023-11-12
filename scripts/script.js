d3dab01957567fcedc8cc1de0a830e93162ad2cf
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-11-12 11:47:49 -0800

    link login, index, profile, main, navbar and footer home

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,12 @@
 // Call this function when the "logout" button is clicked
 //-------------------------------------------------
 function logout() {
-    firebase.auth().signOut().then(() => {
-        // Sign-out successful.
-        console.log("logging out user");
-      }).catch((error) => {
-        // An error happened.
-      });
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+    console.log("logging out user");
+  }).catch((error) => {
+    // An error happened.
+  });
 }
 
 //------------------------------------------------------------------------------
@@ -36,4 +36,4 @@ function displayCommentsDynamically(collection) {
     })
 }
 
-displayCommentsDynamically("comments");  //input param is the name of the collection
+// displayCommentsDynamically("comments");  //input param is the name of the collection
