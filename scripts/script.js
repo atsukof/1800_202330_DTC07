047d1fdb3d328135209a5224159e1193f85aa686
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-11-12 12:00:33 -0800

    Move displayCommentsDynamically to listing.js

--- scripts/script.js
+++ scripts/script.js
@@ -10,30 +10,3 @@ function logout() {
     // An error happened.
   });
 }
-
-//------------------------------------------------------------------------------
-// Input parameter is a string representing the collection we are reading from
-//------------------------------------------------------------------------------
-function displayCommentsDynamically(collection) {
-  let commentTemplate = document.getElementById("commentTemplate"); // Retrieve the HTML element with the ID "commentTemplate" 
-
-  db.collection(collection).get()   //the collection called "comments"
-    .then(allComments => {
-      allComments.forEach(doc => { //iterate thru each doc
-        var commenter = doc.data().comment_user_ID;
-        var commentDate = doc.data().comment_date;
-        var commentText = doc.data().comment_text;
-        let newcard = commentTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
-
-        //update comment name, date, text
-        newcard.querySelector('.commenter-name').innerHTML = commenter;
-        newcard.querySelector('.comment-date').innerHTML = commentDate.toDate().toLocaleString();
-        newcard.querySelector('.comment-text').innerHTML = commentText;
-
-        document.getElementById(collection + "-go-here").appendChild(newcard);
-
-      })
-    })
-}
-
-// displayCommentsDynamically("comments");  //input param is the name of the collection
