ff7fd1fe51d0426b4f9ba0957194e3c54b07cd24
Atsuko Uemura <atsukof.kt@gmail.com>
2023-11-13 11:05:01 -0800

    Complete watchlist.html and watchlist.js

--- scripts/watchlist.js
+++ scripts/watchlist.js
@@ -25,20 +25,21 @@ function newCard(itemID) {
     console.log("inside new card", itemID)
     doc = db.collection("items").doc(itemID).get().then((doc) => {
         if (doc.exists) {
-            var itemName = doc.data()["name"]
-            var itemLocation = doc.data()["city"]
-            var itemPrice = doc.data()["price"]
-            console.log(itemName, itemLocation, itemPrice)
+            var itemName = doc.data().name
+            var itemLocation = doc.data().location
+            var itemPrice = doc.data().price
+            var itemImg = doc.data().image
+            console.log(itemName, itemLocation, itemPrice, itemImg, itemID)
 
             let newcard = watchlistTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
             //update comment name, date, text
-            // newcard.querySelector('.watchilist-img').innerHTML = img;
-            newcard.querySelector('.price').innerHTML = itemPrice;
+            newcard.querySelector('.img-thumbnail').src = itemImg;
+            newcard.querySelector('.price').innerHTML = `\$${itemPrice}`;
             newcard.querySelector('.location').innerHTML = itemLocation;
+            newcard.querySelector('.listing-link').href = `listing.html?docID=${itemID}`;
 
             document.getElementById("watchlists-go-here").appendChild(newcard);
-
 
         }
     })
