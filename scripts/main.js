15c736ba8bd50a7b28718c6d0beed4dd557fd4e4
Alice Huang <alice.in.realityland@gmail.com>
2023-12-07 14:08:37 -0800

    Decompose functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,9 @@
-// read_items
-async function add_4_card() {
+async function add_6_card() {
+    // Adds 6 cards to the main page
     const results_arr = [];
     await db.collection('items').where('status', '==', 'active').orderBy(`date_created`, `desc`).get()
         .then(
             all_items => {
-
                 all_items.forEach(doc => { //iterate thru each doc
                     var item_obj = {
                         id: doc.id,
@@ -13,52 +12,54 @@ async function add_4_card() {
                         image: doc.data().image,
                         name: doc.data().name
                     };
-
-                    // console.log(item_obj)
                     results_arr.push(item_obj);
                     return results_arr
                 })
                 // add card into main.html
                 for (let i = 0; i < 6; i++) {
-                    let redirect = document.createElement('a')
-                    redirect.href = `listing.html?docID=${results_arr[i].id}`
-
-                    let panel = document.createElement('div')
-                    panel.className = 'panel col'
-
-                    let search = document.createElement('div')
-                    search.className = 'search'
-
-                    let image = document.createElement('img')
-                    image.src = results_arr[i].image
-                    image.className = 'img-thumbnail'
-
-                    let price = document.createElement('p')
-                    price.className = 'price'
-                    price.innerHTML = `$${results_arr[i].price}`
-
-                    let location = document.createElement('p')
-                    location.innerHTML = results_arr[i].location
-                    location.className = 'location'
-
-                    let name = document.createElement('p')
-                    name.innerHTML = results_arr[i].name
-                    name.className = 'name'
-    
-                    redirect.appendChild(image)
-                    search.appendChild(redirect)
-                    search.appendChild(price)
-                    search.appendChild(location)
-                    panel.appendChild(search)
-                    panel.appendChild(name)
-                    document.getElementById('results').appendChild(panel)
-
+                    dynamicallyGenerateElement(results_arr[i])
                 }
             })
 }
 
-// setup
+function dynamicallyGenerateElement(queryResult) {
+    // Dynamically generates an item card from the search result
+    let redirect = document.createElement('a')
+    redirect.href = `listing.html?docID=${queryResult.id}`
+
+    let panel = document.createElement('div')
+    panel.className = 'panel col'
+
+    let search = document.createElement('div')
+    search.className = 'search'
+
+    let image = document.createElement('img')
+    image.src = queryResult.image
+    image.className = 'img-thumbnail'
+
+    let price = document.createElement('p')
+    price.className = 'price'
+    price.innerHTML = `$${queryResult.price}`
+
+    let location = document.createElement('p')
+    location.innerHTML = queryResult.location
+    location.className = 'location'
+
+    let name = document.createElement('p')
+    name.innerHTML = queryResult.name
+    name.className = 'name'
+
+    redirect.appendChild(image)
+    search.appendChild(redirect)
+    search.appendChild(price)
+    search.appendChild(location)
+    panel.appendChild(search)
+    panel.appendChild(name)
+    document.getElementById('results').appendChild(panel)
+}
+
 function setup() {
+    // Runs when the page is loaded
     console.log("main.js is loaded")
     add_4_card()
 
@@ -72,13 +73,14 @@ function setup() {
     })
 }
 
-
 function searchQuery() {
+    // Stores the search query in session storage
     keywords = $("#search-input").val().split(" ")
     sessionStorage.setItem('keywords', keywords);
 }
 
 function searchByType(event) {
+    // Stores the type of item to search for in session storage
     var selectedType = event.currentTarget.id
     sessionStorage.setItem("type", selectedType)
 }
